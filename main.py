b5844e5a0e09af39867d23b8098ad82895d70484
Seascouter <seascouterdev@icloud.com>
2022-05-26 22:51:27 -0400

    Edited main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['S', 'C', 'H', 'D']
 
 players = {}
+stillIn = {}
+order = []
 
 # FUNCTIONS
 def create_deck(d):
@@ -19,5 +21,7 @@ def init_players():
     for player in players:
         if players[player] == 'joining':
             players[player] = hands.Player()
+        order.append(player)
+        stillIn[player] = True
 
 # MAIN CODE
