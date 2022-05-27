410212d550bbefb29f4e32e19b0e0e819803dc8d
Seascouter <seascouterdev@icloud.com>
2022-05-26 22:13:59 -0400

    Edited client.py

--- client/client.py
+++ client/client.py
@@ -2,51 +2,110 @@ import socket
 import select
 import errno
 import sys
+import time
 
 HEADER_LENGTH = 10
-IP = '192.168.7.64'
+IP = '192.168.7.240'
 PORT = 44421
 
-my_username = input('Username: ')
-my_password = input('Password: ')
+loggedIn = False
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
+waiting = False
 
-client_socket.setblocking(False)
 
-login = f"un,{my_username}|pw,{my_password}".encode()
-loginHeader = f"{len(login):<{HEADER_LENGTH}}".encode()
-client_socket.send(loginHeader+login)
+def command_parse(msg):
+    msg = msg.split("|")
+    for i in range(len(msg)):
+        msg[i] = msg[i].split(",")
+    return msg
+
+
+
+while loggedIn is False:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((IP, PORT))
+
+    client_socket.setblocking(False)
+    my_username = input('Username: ')
+    my_password = input('Password: ')
+
+    login = f"un,{my_username}|pw,{my_password}".encode()
+    loginHeader = f"{len(login):<{HEADER_LENGTH}}".encode()
+    client_socket.send(loginHeader+login)
+
+    try:
+        while True:
+            time.sleep(0.25)
+            lnmessage_header = client_socket.recv(HEADER_LENGTH)
+
+            if not len(lnmessage_header):
+                print("Connection closed by the server")
+                sys.exit()
+            lnmessage_length = int(lnmessage_header.decode().strip())
+            lnmessage = client_socket.recv(lnmessage_length).decode()
+            if lnmessage == 'sl':
+                loggedIn = True
+                break
+            elif lnmessage == 'wl':
+                continue
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+
+        if loggedIn is False:
+            continue
+        else:
+            break
+    except Exception as e:
+        print('Reading error: {}'.format(str(e)))
+        sys.exit()
+
 
 
 while True:
-    message = input(f'{my_username} >  ')
 
-    if message:
-        message = message.encode()
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode()
-        client_socket.send(message_header + message)
-        try:
-            while True:
-                username_header = client_socket.recv(HEADER_LENGTH)
-                if not len(username_header):
-                    print("Connection closed by the server")
-                    sys.exit()
-                username_length = int(username_header.decode().strip())
-                username = client_socket.recv(username_length).decode()
+    if waiting is False:
+        omessage = input(f'{my_username} >  ')
+    else:
+        omessage = 'no'
 
-                message_header = client_socket.recv(HEADER_LENGTH)
-                message_length = int(message_header.decode().strip())
-                message = client_socket.recv(message_length).decode()
+    if omessage != 'no':
+        omessage = omessage.encode()
+        omessage_header = f"{len(omessage):<{HEADER_LENGTH}}".encode()
+        client_socket.send(omessage_header + omessage)
 
-                print(f'{username} >  {message}')
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error: {}'.format(str(e)))
+    else:
+        pass
+
+    try:
+        while True:
+            time.sleep(0.25)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            if not len(message_header):
+                print("Connection closed by the server")
                 sys.exit()
+            message_length = int(message_header.decode().strip())
+            message = client_socket.recv(message_length).decode()
+            message = command_parse(message)
 
-            continue
-        except Exception as e:
+            if message[0][0] == 'wt':
+                if message[0][1] == my_username:
+
+                    if waiting is True:
+                        waiting = False
+                    else:
+                        waiting = True
+
+            print(f'{message[0][2]}')
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error: {}'.format(str(e)))
             sys.exit()
+
+        continue
+    except Exception as e:
+        print('Reading error: {}'.format(str(e)))
+        sys.exit()
